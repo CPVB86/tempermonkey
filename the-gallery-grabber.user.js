adefaee7e31e6296525b8213fe57ca0c6cbce55f
CPVB86 <chantor@gmail.com>
2025-06-17 16:18:46 +0200

    Update the-gallery-grabber.user.js

--- the-gallery-grabber.user.js
+++ the-gallery-grabber.user.js
@@ -2,12 +2,12 @@
 // @name         🧙‍♂️ The Gallery Grabber
 // @version      2.1
 // @description  Download alle 1200px productfoto's van Wacoal los en netjes, met statusknop rechtsboven.
+// @match        https://b2b.wacoal-europe.com/*
+// @grant        none
 // @namespace    https://runiversity.nl
 // @author       C. P. v. Beek
 // @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/the-gallery-grabber.user.js
 // @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/the-gallery-grabber.user.js
-// @match        https://b2b.wacoal-europe.com/*
-// @grant        none
 // ==/UserScript==
 
 (function () {
