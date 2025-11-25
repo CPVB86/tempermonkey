8b0e4122ec0efb658aec91b661453468aa0ddb27
CPVB86 <chantor@gmail.com>
2025-11-25 16:49:11 +0100

    Update sparkle-lingadore.user.js

--- sparkle/sparkle-lingadore.user.js
+++ sparkle/sparkle-lingadore.user.js
@@ -6,8 +6,8 @@
 // @grant        none
 // @run-at       document-idle
 // @author       C. P. v. Beek
-// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-lingadore.user.js
-// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-lingadore.user.js
+// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-lingadore.user.js
+// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-lingadore.user.js
 // ==/UserScript==
 
 (function () {
