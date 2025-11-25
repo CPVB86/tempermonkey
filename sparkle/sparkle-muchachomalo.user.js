15865fb88f703a03798ffbe8364a3095c5bdb606
CPVB86 <chantor@gmail.com>
2025-11-25 16:47:57 +0100

    Update sparkle-muchachomalo.user.js

--- sparkle/sparkle-muchachomalo.user.js
+++ sparkle/sparkle-muchachomalo.user.js
@@ -6,8 +6,8 @@
 // @grant        none
 // @run-at       document-idle
 // @author       C. P. v. Beek
-// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-muchachomalo.user.js
-// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-muchachomalo.user.js
+// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-muchachomalo.user.js
+// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-muchachomalo.user.js
 // ==/UserScript==
 
 (function () {
