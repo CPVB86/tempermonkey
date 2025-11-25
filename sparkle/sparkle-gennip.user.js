dd84ea8a4fa8ed1062bb7a1d133b56c20cad7e1a
CPVB86 <chantor@gmail.com>
2025-11-25 16:48:02 +0100

    Update sparkle-gennip.user.js

--- sparkle/sparkle-gennip.user.js
+++ sparkle/sparkle-gennip.user.js
@@ -6,8 +6,8 @@
 // @grant        none
 // @run-at       document-idle
 // @author       C. P. v. Beek
-// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-gennip.user.js
-// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle-gennip.user.js
+// @updateURL    https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-gennip.user.js
+// @downloadURL  https://raw.githubusercontent.com/CPVB86/tempermonkey/main/sparkle/sparkle-gennip.user.js
 // ==/UserScript==
 
 (function () {
